1b54ffec5c91df9137554c8b872f8b1535959768
sist <sist@sist-210>
2018-10-12 17:35:09 +0900

    question controller 수정

--- lumos/src/main/java/com/sp/customerCenter/question/QuestionController.java
+++ lumos/src/main/java/com/sp/customerCenter/question/QuestionController.java
@@ -219,8 +219,10 @@ public class QuestionController {
 			return ".customerCenter.error";
 		}
 		String str="["+dto.getTitle()+"] 에 대한 관리자의 답변입니다. \n";
-		dto.setContent(str);
-		
+		dto.setTitle(str);
+		List<Questions> listCategory = new ArrayList<>();
+		listCategory=service.listCategory();
+		model.addAttribute("listCategory",listCategory);
 		model.addAttribute("dto",dto);
 		model.addAttribute("page",page);
 		model.addAttribute("mode","reply");
@@ -243,7 +245,6 @@ public class QuestionController {
 		
 		Map<String, Object> model=new HashMap<>();
 		model.put("state", state);
-
 		return model;
 	}
 	
